3be53016dc529b5c6568e37a61eff20520215317
YACRUZ <yamatsukino@gmail.com>
2023-04-21 23:50:30 -0600

    create branch base

---
+++ src/components/LinkList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from './Link';
+
+const LinkList = () => {
+  const linksToRender = [
+    {
+      id: 'link-id-1',
+      description:
+        'Prisma gives you a powerful database toolkit 😎',
+      url: 'https://prisma.io'
+    },
+    {
+      id: 'link-id-2',
+      description: 'The best GraphQL client',
+      url: 'https://www.apollographql.com/docs/react/'
+    },
+    {
+        id: 'link-id-3',
+        description: 'The best Anime page',
+        url: 'https://beta.crunchyroll.com/es/'
+      }
+  ];
+
+  return (
+    <div>
+      {linksToRender.map((link) => (
+        <Link key={link.id} link={link} />
+      ))}
+    </div>
+  );
+};
+
+export default LinkList;
