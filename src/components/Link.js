f69c1ce681dead1d56be1f621e575d7dfe4151d5
YACRUZ <yamatsukino@gmail.com>
2023-05-01 12:25:09 -0600

    create front #1

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Link = (props) => {
-  const { link } = props;
+const Link = ({ link }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
-    <div>
-      <div>
-        {link.titulo} ({link.temporada}) ({link.genero}) ({link.capitulos}) ({link.estudio}) ({link.director}) ({link.animacion})
-        ({link.formato}) ({link.adaptacion})
-      </div>
+    <div className='text-center'>
+      <div onClick={toggleDetails} style={{ cursor: 'pointer' }}>
+        <button className="buttontitle">{link.titulo}</button>
+      </div >
+      {showDetails && (
+        <table className='cont-center'>
+          <tbody>
+            <tr>
+              <td>Temporada:</td>
+              <td>{link.temporada}</td>
+            </tr>
+            <tr>
+              <td>Género:</td>
+              <td>{link.genero}</td>
+            </tr>
+            <tr>
+              <td>Capítulos:</td>
+              <td>{link.capitulos}</td>
+            </tr>
+            <tr>
+              <td>Estudio:</td>
+              <td>{link.estudio}</td>
+            </tr>
+            <tr>
+              <td>Director:</td>
+              <td>{link.director}</td>
+            </tr>
+            <tr>
+              <td>Animación:</td>
+              <td>{link.animacion}</td>
+            </tr>
+            <tr>
+              <td>Formato:</td>
+              <td>{link.formato}</td>
+            </tr>
+            <tr>
+              <td>Adaptación:</td>
+              <td>{link.adaptacion}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
